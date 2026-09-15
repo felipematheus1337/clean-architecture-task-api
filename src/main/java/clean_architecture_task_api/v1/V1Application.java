10fe8b5ee78f03c2a49e8d352c3d4a4ace90f22a
felipematheus1337 <lipehbr@gmail.com>
2026-09-15 14:34:44 -0300

    Project created.

---
+++ src/main/java/clean_architecture_task_api/v1/V1Application.java
@@ -0,0 +1,13 @@
+package clean_architecture_task_api.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V1Application.class, args);
+	}
+
+}
---
+++ src/test/java/clean_architecture_task_api/v1/V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package clean_architecture_task_api.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
